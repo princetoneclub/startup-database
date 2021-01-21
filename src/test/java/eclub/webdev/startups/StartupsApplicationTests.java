7cd192cd42cdc8d53cea5aa97155f3b17a549e9b
chuckan13 <an.charlie@gmail.com>
2021-01-20 19:00:21 -0500

    sdf

--- src/test/java/eclub/webdev/startups/StartupsApplicationTests.java
+++ src/test/java/eclub/webdev/startups/StartupsApplicationTests.java
@@ -1,13 +1,16 @@
 package eclub.webdev.startups;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class StartupsApplicationTests {
+public class StartupsApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
